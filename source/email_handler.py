07c04b32826a2a10509d41ce6d63f096c0accd10
Bivtor <victorinaldi@ucla.edu>
2023-04-09 15:11:51 -0700

    Massive file organization overhaul + Added proper logging + Finished auto upload

---
+++ source/email_handler.py
@@ -0,0 +1,299 @@
+import re
+import sys
+import os
+import json
+import base64
+
+from auto_candidate import DEFAULT_SHEET_DEST,JOBMAPFILEPATH,SPREADSHEET_ID, NAMESFILEPATH,creds, build, HttpError, Data, logger
+from auto_candidate import checkSheetNameValidity, setColumnVariables
+
+def getRawDataInfo(raw_message: str) -> dict:
+    """
+    returns a dict object with the candide info we need with the keys:
+        candidate_name
+        candidate_url
+        sheet_destination
+
+        Only call this function after using checkIsCandidateSubmissionNotice()
+    """
+    results:dict = {}
+    
+    # Get URL
+    pattern = r'https://www\.ziprecruiter\.com/contact/response/[^/]*/[^/]*\?'
+    match = re.search(pattern, raw_message)
+    candidate_url = match.group(0)
+    results['candidate_url'] = candidate_url
+    logger.info(f'Got URL : {candidate_url}')
+
+    # Get candidate name
+    loc = raw_message.find("New Candidate:")
+    candidate_name = raw_message[loc+15:loc+100]
+    candidate_name = candidate_name[:candidate_name.find(
+        "for")].strip()
+    results['candidate_name'] = candidate_name
+    logger.info(f'Got Name : {candidate_name}')
+
+    # Get job title
+    job_title_area_location = raw_message.find("New Candidate:")
+    job_title = raw_message[job_title_area_location:job_title_area_location+100]
+    job_title = job_title[job_title.find("'")+1:]
+    job_title = job_title[:job_title.find("'")].lower().strip()
+    logger.info(f'Got Job Title : {job_title}')
+    
+    # Get sheet destination based on job title
+    sheet_destination = handleJobTitle(job_posting_title=job_title)
+    results['sheet_destination'] = sheet_destination
+    # Result logged in function
+
+    return results
+
+
+def handleJobTitle(job_posting_title: str) -> str:
+    """
+        Converts the job posting title to the title of a sheet destination that is in the google sheets
+    """
+    logger.info('Attempting to retrieve sheet destination')
+
+    # Create checkExistence function to be used 
+    def checkLocalExistence(data: dict) -> str:
+        if job_posting_title in data:
+            logger.info(f'Found appropriate sheet destination: {data[job_posting_title]}')
+            return data[job_posting_title]
+        else:
+            logger.info(f'Could not find appropriate sheet desination: {DEFAULT_SHEET_DEST}')
+            return DEFAULT_SHEET_DEST
+        
+    if os.path.isfile(JOBMAPFILEPATH):
+        # If it exists open it
+        with open(JOBMAPFILEPATH, 'r') as f:
+            data = json.load(f) # Load data
+            return checkLocalExistence(data) # Check if the sheet_name exists in the json
+    else:
+        # If file does not exist, create it
+        logger.info(f'Could not find \'{JOBMAPFILEPATH}\', creating one (This must be filled in by hand)') 
+        
+        with open(JOBMAPFILEPATH, 'w') as f:
+            json.dump({}, f)
+        
+        # Log creation of file and return default
+        logger.info(f'Returning default sheet_destination {DEFAULT_SHEET_DEST}') 
+        return DEFAULT_SHEET_DEST
+
+
+def process_message():
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+
+        # Get list of last (1) message ID's
+        messages = service.users().messages().list(
+            userId='me', maxResults=1).execute()
+
+        # Get ID of last message
+        message = messages['messages'][0]['id']
+
+        # Get raw_data of last message (Becasue the raw data contains the url we want and not the non raw for some reason)
+        message = service.users().messages().get(
+            userId='me', id=message, format='raw').execute()
+
+        # Decode raw_data
+        raw_message = base64.urlsafe_b64decode(
+            message['raw'].encode('utf-8')).decode('utf-8')
+        
+        # Log
+        logger.info('Checking most recent email')
+        
+        # Check if is the right email (ZR Notification)
+        
+        if not checkIsCandidateSubmissionNotice(raw_message=raw_message): 
+            logger.info('Notification was not for candidate submission, returning')
+            return 200
+
+        # Log
+        logger.info('Notification was for candidate submission, gathering data')
+
+        # Gather data from email before checking if candidate exists
+        parsed_email_data:dict = getRawDataInfo(raw_message=raw_message)
+        
+        # Extract data from dict
+        candidate_url = parsed_email_data['candidate_url']
+        candidate_name = parsed_email_data['candidate_name']
+        sheet_destination = parsed_email_data['sheet_destination']
+
+        # Check if candidate already exists
+        if checkCandidateExistence(SPREADSHEET_ID, sheet_name=sheet_destination, name=candidate_name):
+            logger.info('Candidate already exists, returning')
+            return 200
+        
+        # We know the candidate is not in the spreadsheet, add the candidate (updates json at end of function)
+        logger.info(f'Candidate is new, Now adding to them to the {sheet_destination} sheet')
+
+        # Call clone of submit data (checks password, isRunning, columnVariables, etc)
+        final_candidate_data = Data(action='Add', category=sheet_destination,
+                        link=candidate_url, password='gabeandcara2023', message="")
+
+        submitdata_clone(data=final_candidate_data)  # Add Candidate
+
+        logger.info('Submitted to spreadsheet, check results')
+
+        # TODO def sendAWSEmail(name: str, email: str, body: str, category: str, mailsender):
+        # TODO Alert Victor of additon
+
+        # TODO Get the last line of the spreadsheet
+        # TODO Call the send text function for only the last line of the spreadsheet
+
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+
+
+def checkIsCandidateSubmissionNotice(raw_message: str)-> bool:
+    # Check if the text contains a URL in the format "https://www.ziprecruiter.com/contact/response/*/*"
+    pattern = r'https://www\.ziprecruiter\.com/contact/response/[^/]*/[^/]*\?'
+    match = re.search(pattern, raw_message)
+    if match: 
+        
+        return True
+    else: 
+        return False
+
+
+def checkCandidateExistence(spreadsheet_id, sheet_name, name) -> True:
+    """
+    Check if the candidate already exists in the spreadsheet, in the spreadsheet, we only call this function if the top 
+    email is a notification of a new candidate, and we use this function to prevent unnecessary calls to the API by using a local
+    storage of the names, and only calling the API if we have to (ie when the job sheet does not exist locally)
+
+    We update the local file every time a new request is made, so updates are only called from here if the sheet does not exist
+    """
+
+    # Check if file exists
+    if os.path.isfile(NAMESFILEPATH):
+        # If it exists open it
+        with open(NAMESFILEPATH, 'r') as f:
+            data = json.load(f)
+
+            # Check if the sheet_name exists in the json
+            if sheet_name in data:
+                if name in data[sheet_name]:  # Return True if we find the name in the file
+                    return True
+                else:
+                    return False
+            # Update the spreadsheet if we can't find the name the sheet_name
+            else:
+                updateCandidateExistence(
+                    spreadsheet_id=spreadsheet_id, sheet_name=sheet_name)
+                with open(NAMESFILEPATH, 'r') as f:
+                    data = json.load(f)
+                    # Try to find the name again
+                    # Return True if we find the name in the file
+                    if name in data[sheet_name]:
+                        return True
+                    else:
+                        return False
+    # If file does not exist
+    else:
+        # Create file
+        with open(NAMESFILEPATH, 'w') as f:
+            json.dump({}, f)
+
+        # Update the file
+        updateCandidateExistence(
+            spreadsheet_id=spreadsheet_id, sheet_name=sheet_name)
+
+        # Try to find the name again
+        with open(NAMESFILEPATH, 'r') as f:
+            data = json.load(f)
+            if name in data[sheet_name]:  # Return True if we find the name in the file
+                return True
+            else:
+                return False
+
+
+def updateCandidateExistence(spreadsheet_id, sheet_name):
+    try:
+        # Authorize with Google Sheets API
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Get the values in the column
+        RANGE = f"{sheet_name}!A:A"
+
+        # Execute request
+        result = service.spreadsheets().values().get(
+            spreadsheetId=spreadsheet_id, range=RANGE).execute()
+
+        # Get values
+        column_values = result.get("values", [])
+
+        # Remove arbirary list within list
+        column_values = [val for sublist in column_values for val in sublist]
+
+        # Update the value in the json file
+        with open(NAMESFILEPATH, "r") as f:
+            data = json.load(f)
+
+        data[sheet_name] = column_values
+
+        with open(NAMESFILEPATH, 'w') as f:
+            json.dump(data, f, indent=4)
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+
+
+def submitdata_clone(data: Data):
+    # Check if operation is currently in progress
+    f = open('isWorking.json')
+    json_check = json.load(f)
+    f.close()
+
+    # If there is a current process, return checkModel with its"not_working_response"
+    if json_check['isWorking']:
+        # TODO Indicate that we were working and could not add a candidate
+        print("Could not add candidate")
+        return 200
+
+    # Set that there is now an operation ongoing since we passed the check
+    json_check['isWorking'] = True
+    with open("isWorking.json", "w") as outfile:
+        json.dump(json_check, outfile)
+
+    # Check Password
+    if data.password != os.environ['DEFAULT_PASSWORD']:
+        return "Incorrect Password"
+
+    # Check Validity
+    checkSheetNameValidity(data.category, data)
+    if ((not data.isFolder) and (not data.isSheet)):
+        return "Folder or Sheet name does not exactly match category"
+
+    # Set Variables
+    setColumnVariables(data)
+
+    # Write data to a file for use if adding candidates
+    with open("settings.json", "w") as outfile:
+        outfile.write(data.json())
+    print("Successfully Set Column Variables")
+
+    ################## Passed Pre-Checks ##################
+    if data.action == "Add":
+        openstring_clone(data)
+
+    # Stop Working
+    json_check['isWorking'] = False
+    with open("isWorking.json", "w") as outfile:
+        json.dump(json_check, outfile)
+
+    # Always return a value
+    return 200
+
+def openstring_clone(data: Data):
+    print("Opening Link: {}".format(data.link))
+    cmd = 'start chrome {}'.format(data.link)  # OPEN chrome
+    if os.system(cmd) != 0:
+        return False
+
+def main():
+    print("test")
+
+if __name__ == '__main__':
+    main()
+
---
+++ source/server.py
@@ -0,0 +1,174 @@
+from auto_candidate import *
+from email_handler import process_message, updateCandidateExistence
+
+from pydantic import BaseModel
+import os
+import json
+import base64
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from hashlib import sha256
+from auto_candidate import logger
+
+# Log Server start
+logger.info('Started Server')
+
+load_dotenv()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class textData(BaseModel):
+    category_texts: str
+
+
+def openstring(data: Data):
+    print("Opening Link: {}".format(data.link))
+    cmd = 'start chrome {}'.format(data.link)  # OPEN chrome
+    if os.system(cmd) != 0:
+        return False
+
+
+@app.post('/runfunction')
+def createUsers(candidateData: candidateData):
+    f = open('settings.json')
+    data = json.load(f)
+    # Add candidate with saved data
+    create_candidate(candidateData, data)
+    f.close()
+    
+    # Log
+    logger.info(f'Created profile for {candidateData.name}')
+
+    # Update Candidate Existence
+    updateCandidateExistence(
+        SPREADSHEET_ID, sheet_name=data['category'])
+    
+    # Log
+    logger.info(f'Updated local json file for {candidateData.name}')
+
+    return {"message": "Success"}
+
+
+class ResponseModel(BaseModel):
+    response: str
+
+
+@app.post('/submitdata')
+def submitdata(data: Data):
+    # Check if operation is currently in progress
+    f = open('isWorking.json')
+    json_check = json.load(f)
+    f.close()
+
+    # Check Password
+    if data.password != os.environ['DEFAULT_PASSWORD']:
+        return ResponseModel(response="Incorrect Password")
+
+    # If there is a current process, return checkModel with its"not_working_response"
+    if json_check['isWorking']:
+        return ResponseModel(response="Operation Ongoing")
+
+    # Set that there is now an operation ongoing since we passed the check
+    json_check['isWorking'] = True
+    with open("isWorking.json", "w") as outfile:
+        json.dump(json_check, outfile)
+
+    # Check Validity
+    checkSheetNameValidity(data.category, data)
+    if ((not data.isFolder) and (not data.isSheet)):
+        return "Folder or Sheet name does not exactly match category"
+
+    # Set Variables
+    setColumnVariables(data)
+
+    # Write data to a file for use if adding candidates
+    with open("settings.json", "w") as outfile:
+        outfile.write(data.json())
+    print("Successfully Set Column Variables")
+
+    ################## Passed Pre-Checks ##################
+    if data.action == "Text":
+        sendmailtexts(data)
+    if data.action == "Add":
+        openstring(data)
+
+    # Stop Working
+    json_check['isWorking'] = False
+    with open("isWorking.json", "w") as outfile:
+        json.dump(json_check, outfile)
+
+    return ResponseModel(response="Success")
+
+
+class MailData(BaseModel):
+    message: dict
+
+
+@app.post('/retrieve_email')
+def createUsers(data: MailData):
+    # Decode message
+    response = base64.b64decode(data.message['data'])
+
+    # Convert bytes to string using `decode()` method
+    response_str = response.decode('utf-8')
+
+    # Convert JSON string to Python object using `json.loads()` function
+    response_json = json.loads(response_str)
+    logger.info('Recieved New Email notification')
+
+    # Get message
+    process_message()
+
+    return 200
+
+
+
+
+
+
+def prevent_duplicates(inputname: str):
+
+    # Check if the file exists
+    if os.path.isfile(NAMESFILEPATH):
+        # File exists, load the JSON data
+        with open(NAMESFILEPATH, "r") as json_file:
+            data = json.load(json_file)
+            # Check if the 'names' key exists and contains a string
+            if inputname in data['names']:
+                return True
+            else:
+                return False
+    else:
+        # File does not exist, create the JSON file and add the names from the database
+        # TODO: Add code to populate the 'names' list
+
+        # Save the JSON data to the file
+        with open(NAMESFILEPATH, "w") as json_file:
+            json.dump(data, json_file)
+
+
+
+
+
+@app.post('/sendtexts')
+def sendtexts():
+    f = open('settings.json')
+    data = json.load(f)
+    sendmailtexts(data)
+    f.close()
+    return {"Success"}
