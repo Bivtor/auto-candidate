d9945800f3bea491a7be5d8717bb86257f0aee10
Victor Rinaldi <victorinaldi@ucla.edu>
2023-06-19 13:44:59 -0700

    prisma schema

--- source/Interact.py
+++ source/Interact.py
@@ -1,27 +1,32 @@
 from dotenv import load_dotenv
-from auto_candidate import ENV_PATH
 import os
 import asyncio
 from prisma import Prisma
 
-
-load_dotenv(dotenv_path=ENV_PATH)
-
+load_dotenv(dotenv_path=".env")
 
 
-async def main() -> None:
+async def run() -> None:
     prisma = Prisma()
+
     await prisma.connect()
 
     # write your queries here
-    user = await prisma.user.create(
-        data={
-            'name': 'Robert',
-            'email': 'robert@craigie.dev'
-        },
-    )
+    query = '''
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public';
+    '''
+    result = await prisma.query_raw(query)
+    tables = [row['table_name'] for row in result]
+    print(tables)
 
     await prisma.disconnect()
 
+
+def main():
+    asyncio.run(run())
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
