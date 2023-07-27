031e17dcf38392732c07b185e6f2b49e36a02dfb
Victor Rinaldi <victorinaldi@ucla.edu>
2023-07-27 15:47:47 -0700

    oops

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI2OTM4NzUxOSwiYWFpIjoxMSwidWlkIjo0NTgxNDc2NywiaWFkIjoiMjAyMy0wNy0xOFQwMjo0OTo1My4wNjZaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTc4NjM4NjAsInJnbiI6InVzZTEifQ.6aI2-fuA_ThOX5lXEShu_uGrPbmwYmf6isgI5GuzXO0"
+apiKey = "stop"
 apiUrl = "https://api.monday.com/v2"
 headers = {"Authorization" : apiKey, "API-version" : 2023-04}
 
